2ed5ef7ed8c88667a3d3f76f7ebb173d7e2a8952
caitlin <caitlin.wink@gmail.com>
2021-04-07 08:49:33 -0700

    update

--- src/index.js
+++ src/index.js
@@ -14,32 +14,18 @@ function catFactz() {
 
 let allFactz 
 function renderFactz(data) {
-    allFactz = data
-    for (let i=0; i < allFactz.length; i++) {
-            console.log(`${allFactz[i].text}`)
-    }
-    return renderFactz()
-    // const factz1 = data[0].text
-    // const factz2 = data[1].text
-    // const factz3 = data[2].text
-    // const factz4 = data[3].text
-    // const factz5 = data[4].text
-    
-    //
-    //listed.innerHTML = [factz1 , factz2, factz3, factz4, factz5]
-    //allFactz = [factz1 , factz2, factz3, factz4, factz5]
+    allFactz = data.map(fact => fact.text)
+    console.log(allFactz)
 };
 
 const listed = document.getElementById("yourFactz")
 function givenNumber(e) {
     e.preventDefault();
     let submitter = e.submitter
-    //let givenFact = submitter.text
-
-    if (submitter > 6) {
-        renderFactz()
-    } else {
-        return "Try Again :)"
-    }
+        if (submitter < 6) {
+            renderFactz()
+        } else {
+            return "Try Again :)"
+        }   
 }
-listed.appendChild(givenNumber())
+//listed.appendChild(givenNumber());
